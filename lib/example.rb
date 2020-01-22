de6ef98d8e574a2d902df0c0b9d34e3cd2cd1252
Kevin Villalobos <kevinv15v@gmail.com>
2020-01-22 04:35:53 +0000

    Done.

--- lib/example.rb
+++ lib/example.rb
@@ -1,13 +1,3 @@
-# Use this space to code along with the readme
+$ ruby greeting.rb
+$
 
-phrase = "Hello World!"
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
-puts phrase
